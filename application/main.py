55a53ede1f2b0e986be3662dff34fc261e2dac6a
Fernando Garate <fmgarate@gmail.com>
2019-04-25 23:38:30 -0300

    update main app method, complete basic api endpoints

--- application/main.py
+++ application/main.py
@@ -1,15 +1,21 @@
 import bottle
 from bottle import request
 
-import shortcuts
+import forms
 import models
+import shortcuts
 
 app = bottle.Bottle()
 
 
 @app.get("/api/notes/")
 def note_list():
+    """
+    Notes list api endpoint. Returns a list of notes
+    """
+
     qs = models.Note.query().order(-models.Note.created)
+
     return {
         "items": qs.map(lambda n: n.to_data())
     }
@@ -17,21 +23,55 @@ def note_list():
 
 @app.post("/api/notes/")
 def note_create():
-    return {}
+    """
+    Note create api endpoint. Create a new note
+    """
+
+    form = forms.NoteForm(request.POST)
+
+    if form.validate():
+        note = models.Note()
+        form.populate_obj(note)
+        note.put()
+
+        return note.to_data()
+
+    return {"errors": form.errors}
 
 
 @app.get("/api/notes/<note_id:int>/")
 def note_details(note_id):
+    """
+    Note details api endpoint. Returns a note instance
+    """
     return shortcuts.get_object_or_404(models.Note, note_id).to_data()
 
 
 @app.put("/api/notes/<note_id:int>/")
 def note_update(note_id):
+    """
+    Note update api endpoint. Update a note
+    """
+
     note = shortcuts.get_object_or_404(models.Note, note_id)
-    return {}
+    form = forms.NoteForm(request.POST, note)
+
+    if form.validate():
+        form.populate_obj(note)
+        note.put()
+
+        return note.to_data()
+
+    return {"errors": form.errors}
 
 
 @app.delete("/api/notes/<note_id:int>/")
 def note_delete(note_id):
+    """
+    Note delete api endpoint. Delete a note
+    """
+
     note = shortcuts.get_object_or_404(models.Note, note_id)
+    note.key.delete()
+
     return {}
